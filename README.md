ee108760fbb2ad03a1d7ba6dc80ed55b46c96dfe
turara <turara.developer@gmail.com>
2020-08-16 14:21:50 +0900

    [chore] Initial commit with README.md, .gitignore and initial files of SwiftUI project

---
+++ README.md
@@ -0,0 +1,9 @@
+# SwiftUI and Combine Unit Tests sample
+
+Sample project for unit testing with SwiftUI and Combine.
+
+## Git Commit
+
+### Use prefix
+
+Refer to https://github.com/angular/angular.js/blob/master/DEVELOPERS.md#type
---
+++ SwiftUIAndCombineUnitTestsSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAndCombineUnitTestsSample
+//
+//  Created by Turara on 2020/08/16.
+//  Copyright © 2020 Turara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
